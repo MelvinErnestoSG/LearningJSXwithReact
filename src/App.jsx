8d45b31d725771a7997fd0cc2a1ecc6e4f51435a
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-05-03 14:39:46 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
-function Greeting() {
-  return (
-    <h1 className="texto1">Bienvenidos a todos.</h1>
-  );
-}
-
-function Paragraph() {
-  return (
-    <p className="parrafo">Estoy Aprendiendo React JSX.</p>
-  );
-}
-
-function Registration() {
-  return (
-    <h2 className="texto2">Registration</h2>
-  );
-}
-
-function Button() {
-  return (
-    <Button className="boton">Join the team</Button>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="pie">
-    <p>
-      Copyright &copy; 2021-2022 <br></br>Melvin Ernesto Santana Garcia. <br></br>Todos los derechos reservados.
-    </p>
-  </footer>
-  );
-}
+import Greeting from './Component/Greeting';
+import Paragraph from './Component/Paragraph';
+import Registration from './Component/Registration';
+import Button from './Component/Button';
+import Footer from './Component/Footer';
 
 function App() {
   return (
     <div className="App">
 
-      <header clasName="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     
@@ -47,11 +18,12 @@ function App() {
 
       <Paragraph />{/*Funcion de Parrafo.*/}
 
-      <Registration/>{/*Funcion de Texto h2.*/}
+      <Registration />{/*Funcion de Texto h2.*/}
 
       <Button />{/*Funcion del boton.*/}
 
       <Footer />{/*Funcion de Pie de Pagina.*/}
+      
     </div>
   );
 }
