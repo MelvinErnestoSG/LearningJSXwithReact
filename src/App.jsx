5009366e2b3081e0cd48157d81aace0d3fdd3e3a
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-05-03 02:12:35 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
-function greeting(text) {
-  return <h1>{text}</h1>;
+function Greeting() {
+  return (
+    <h1 className="texto1">Bienvenidos a todos.</h1>
+  );
 }
 
-function paragraph(text){
-  return <p>{text}</p>;
+function Paragraph() {
+  return (
+    <p className="parrafo">Estoy Aprendiendo React JSX.</p>
+  );
+}
+
+function Registration() {
+  return (
+    <h2 className="texto2">Registration</h2>
+  );
+}
+
+function Button() {
+  return (
+    <Button className="boton">Join the team</Button>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="pie">
+    <p>
+      Copyright &copy; 2021-2022 <br></br>Melvin Ernesto Santana Garcia. <br></br>Todos los derechos reservados.
+    </p>
+  </footer>
+  );
 }
 
 function App() {
   return (
     <div className="App">
+
       <header clasName="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {greeting('Hello JSX!')}
-        {paragraph("Estoy Aprendiendo React JSX")}
       </header>
+    
+      <Greeting />{/*Funcion de Saludar Texto h1.*/}
+
+      <Paragraph />{/*Funcion de Parrafo.*/}
+
+      <Registration/>{/*Funcion de Texto h2.*/}
+
+      <Button />{/*Funcion del boton.*/}
+
+      <Footer />{/*Funcion de Pie de Pagina.*/}
     </div>
   );
 }
