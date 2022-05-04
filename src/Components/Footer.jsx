93b4822c9c0435dbce531aae05154f613f365703
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-05-03 23:56:33 -0500

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import '../Styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="PieDePagina">
+    <p>
+      Copyright &copy; 2021-2022 <br></br>Melvin Ernesto Santana Garcia. <br></br>Todos los derechos reservados.
+    </p>
+  </footer>
+  );
+}
+
+export default Footer;
